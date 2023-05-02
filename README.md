7dd71ab98779b55fa44699fc9e38b67ea77177ef
pakzadR <pakzad.rania@gmail.com>
2023-05-02 19:25:52 +0300

    food order

---
+++ README.md
@@ -0,0 +1,76 @@
+# foodorder
+foodorder Online
+
+
+
+## Content
+
+Online ordering consists of admin and user 
+Restaurant Foods Each restaurant has several foods
+Admin can add restaurants and food and in which restaurants 
+User can order food to see their restaurants and food
+
+## ناوەڕۆک 
+داواکردنی خواردن لە ڕیگەی ئۆنڵاینەوە
+کە پێک دێت لە ئادمین و ئوسەر رەستورانت خواردنەکان کە هەر
+رێستۆرانتێک چەند خوارنێکی هەیە ئادمین دەتوانێت ریستۆرانت و
+خواردن زیاد بکات وە لە خواردنەکان لە کام رێستۆرانتن ئوسەر
+دەتوانێت داوای خواردن بکات ڕێستۆرانت و خواردنەکانیان ببینێت
+
+
+## Images Home
+## ڕەسمەکانی دەسپێک
+![Screenshot (15)](https://user-images.githubusercontent.com/84478447/230061821-f1125a64-b1cc-443c-b0a7-9668d6877982.png)
+
+![Screenshot (16)](https://user-images.githubusercontent.com/84478447/230062297-8fd9a4ec-3fca-4711-a269-9d50e117c49e.png)
+![Screenshot (17)](https://user-images.githubusercontent.com/84478447/230062328-7bc2c3e6-5f30-4ed1-b5d4-d8e51ca050b1.png)
+![Screenshot (19)](https://user-images.githubusercontent.com/84478447/230063692-bb848908-c48f-4d26-9df2-5e82df1b1d35.png)
+
+
+## Images Resturant
+
+## رەسمی ڕیستورانتەکان
+
+![Screenshot (20)](https://user-images.githubusercontent.com/84478447/230063946-89c2bda5-fff5-4939-9561-ca4795a02723.png)
+
+
+
+## to Admin age 
+go to [localhost/foodoerder/admin](http://localhost:81/foodorder/admin)
+## بۆ کردنەوەی پەیجی ئادمین 
+
+برۆ بۆ لینکی [localhost/foodoerder/admin](http://localhost:81/foodorder/admin)
+
+
+![Screenshot (22)](https://user-images.githubusercontent.com/84478447/230065092-d719c589-c3f2-4ab0-94fe-0aeb7e25da35.png)
+
+## To use Project  
+go to [phpmyadmin](http://localhost:81/phpmyadmin) 
+- Create database by Name 'online_rest'
+- Go to Import on top bar 
+- Chose file 
+- Open database folder in project 
+- select 'online_rest'
+- improve
+- enjoy it 
+
+
+## بۆ بەکار هێنانی پرۆژێجتەکە
+
+بڕۆ بۆ [phpmyadmin](http://localhost:81/phpmyadmin) 
+- داتابەیس درووست بکە بە ناوی  'online_rest'
+-  Import دابگرە کە لە بەشی سەرەوە دیاە 
+- Chose file  هەڵ بژێرە
+- فۆڵدەری داتابەیسەکە بکەوە کە لە پرۆجێکتەکە دا هەیە
+- هەڵبژێرە 'online_rest'
+- لە بەشی خوارەوە imropve داگرە
+- چیژ ببینە
+
+
+
+
+
+
+
+
+
---
+++ footer.php
@@ -0,0 +1,49 @@
+<div class="container">
+
+    <!-- bottom footer statrs -->
+    <div class="bottom-footer">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                <h5>Payment Options</h5>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="images/paypal.png" alt="Paypal"></a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="images/mastercard.png" alt="Mastercard"></a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/maestro.png" alt="Maestro"></a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="images/stripe.png" alt="Stripe"></a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img src="images/bitcoin.png" alt="Bitcoin"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5>Address</h5>
+                                    <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
+                                    <h5>Phone:
+                                        <a >+964 751 708 38 49</a>
+                                    </h5>
+                                    <h5>Phone:
+                                        <a class="" >pakzad.rania@gmail.com</a>
+                                    </h5>
+                                </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5>Addition informations</h5>
+                                    <p>Join the thousands of other restaurants who benefit from having their menus
+                                        on TakeOff</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- bottom footer ends -->
+                    </div>
